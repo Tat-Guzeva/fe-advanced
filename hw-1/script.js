86ecf2eda54a62bbf8204c71c8ec0ea9f809e0dd
Tat-Guzeva <tat.guzeva@gmail.com>
2018-12-03 15:27:13 +0200

    add

--- hw-1/script.js
+++ hw-1/script.js
@@ -1,28 +1,26 @@
 'use strict';
 
-/*const adminLogin = 'admin';
-const adminPassword = 'm4ng0h4ckz';*/
+const adminLogin = 'admin';
+const adminPassword = 'm4ng0h4ckz';
 
-const adminLogin = prompt('Введите логин');
+const MESSAGE_USER_CANCELED = 'Отменено пользователем!';
+const MESSAGE_INVALID_LOGIN = 'Доступ запрещен, неверный логин!';
+const ENTER_PASSWORD = 'Введите пароль';
+const MESSAGE_WELCOME = 'Добро пожаловать!';
 
-const loginCancel = 'Отменено пользователем!';
-const wrongLogin = 'Доступ запрещен, неверный логин!';
-const enterPassword = 'Введите пароль';
-const passwordCancel = 'Отменено пользователем!';
-const wrongPassword = 'Доступ запрещен, неверный пароль!';
-const correctPassword = 'Добро пожаловать!';
+const enter = prompt('Введите логин');
 
-if (!adminLogin){
-  alert(loginCancel);
-}if (adminLogin !== 'admin'){
-  alert(wrongLogin);
-}if (adminLogin === 'admin'){
-  const adminPassword = prompt(enterPassword);
-  if (!adminPassword){
-    alert(wrongPassword);
-  }if (adminPassword !== 'm4ng0h4ckz'){
-    alert(wrongPassword);
-  } if (adminPassword === 'm4ng0h4ckz'){
-    alert(correctPassword);
-  }
+if (!enter){
+  alert(MESSAGE_USER_CANCELED);
+} else if (enter !== adminLogin){
+  alert(MESSAGE_INVALID_LOGIN);
+} else if (enter === adminLogin){
+    const enterPassword = prompt(ENTER_PASSWORD);
+    if (!adminPassword){
+      alert(MESSAGE_USER_CANCELED);
+    } else if (enterPassword !== adminPassword){
+      alert(MESSAGE_INVALID_LOGIN);
+    } else if (enterPassword === adminPassword){
+      alert(MESSAGE_WELCOME);
+    }
 }
