5755e7d5929a524fe5ff943f134a01fa785fd7c2
Tat-Guzeva <tat.guzeva@gmail.com>
2018-12-21 11:10:55 +0200

    hw

--- hw-3/script.js
+++ hw-3/script.js
@@ -3,29 +3,29 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 const min = 4;
 const max = 16;
+const MESSAGE_LOGIN_ADDED = 'Логин успешно добавлен!';
+const MESSAGE_INVALID_LOGIN = 'Логин должен быть от 4 до 16 символов'
+const MESSAGE_LOGIN_USED = 'Такой логин уже используется!';
 
 const isLoginValid = function(login) {
-  if(login.length > min && login.length < max){
-  } else if(login.length < min && login.length > max){
-  }
+  if(login.length < min && login.length > max){
+  } return false;
 };
 
 const isLoginUnique = function(allLogins, login) {
   if(allLogins.includes(login)){
     return false;
-  } else{
-    return true;
   }
 };
 
 const addLogin = function(allLogins, login) {
   if(isLoginValid && isLoginUnique){
     allLogins.push(login);
-    console.log('Логин успешно добавлен!');
-  } if(!isLoginValid(login)){ //если пишу здесь else if, то не работает
-    console.log('Логин должен быть от 4 до 16 символов');
-  } if(!isLoginUnique(allLogins, login)){//если пишу здесь else if, то не работает
-    console.log('Такой логин уже используется!');
+    console.log(MESSAGE_LOGIN_ADDED);
+  } if(!isLoginValid(login)){
+    console.log(MESSAGE_INVALID_LOGIN);
+  } if(!isLoginUnique(allLogins, login)){
+    console.log(MESSAGE_LOGIN_USED);
   }
 };
 
